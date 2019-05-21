ef1174c60bf8f8d0dceff141b81bf31027a9256b
Nicolas Reyland <43726409+Nicolas-Reyland@users.noreply.github.com>
2019-05-21 15:36:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import setuptools
+
+setup(name='matrix',
+      version='1.5',
+      description='Geometry operations',
+      author='Nicolas Reyland',
+      author_email='nicolas@reyland.net',
+      url='https://github.com/Nicolas-Reyland/Snake-Environment',
+      packages=setuptools.find_packages()
+)
